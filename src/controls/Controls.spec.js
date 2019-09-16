c9c2c24b47c6cdf86480c162883ae84937a4511d
XAmi101 <famousvacancy@gmail.com>
2019-09-16 19:35:33 -0400

    MVP

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,44 @@
 // Test away!
+import React from "react";
+import Controls from "./Controls";
+import { fireEvent, render } from "@testing-library/react";
+
+
+describe("<Controls />", () => {
+  it("matches renders without crashing", () => {
+    render(<Controls />);
+  });
+
+  it("open and unlocked", () => {
+    const closeSpy = jest.fn();
+    const lockSpy = jest.fn();
+
+    const { getByText } = render(
+      <Controls
+        closed={true}
+        locked={true}
+        toggleLocked={lockSpy}
+        toggleClosed={closeSpy}
+      />
+    );
+    fireEvent.click(getByText("Unlock Gate"));
+    expect(lockSpy).toHaveBeenCalled();
+  });
+    
+  it("open and unlocked", () => {
+    const closeSpy = jest.fn();
+    const lockSpy = jest.fn();
+
+    const { getByText } = render(
+      <Controls
+        closed={false}
+        locked={false}
+        toggleLocked={lockSpy}
+        toggleClosed={closeSpy}
+      />
+    );
+  fireEvent.click(getByText("Close Gate"));
+      expect(closeSpy).toHaveBeenCalled();
+    });
+
+});
--- src/index.js
+++ src/index.js
@@ -6,3 +6,7 @@ import './index.css';
 import Dashboard from './dashboard/Dashboard';
 
 ReactDOM.render(<Dashboard />, document.getElementById('root'));
+
+
+// yarn remove jest-dom react-testing-library
+// yarn add --dev @testing-library/react @testing-library/jest-dom
